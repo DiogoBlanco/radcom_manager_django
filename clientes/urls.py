9b2cbc9eec184d29c19c8d4f3118a63032a81d70
DiogoBlanco <diogoblanco@outlook.com.br>
2024-02-24 13:01:37 -0300

    feat: edit cliente

--- clientes/urls.py
+++ clientes/urls.py
@@ -6,4 +6,5 @@ app_name = 'clientes'
 urlpatterns = [
     path('', views.customers, name='clientes'),
     path('adicionar/', views.add_customer, name='add_cliente'),
+    path('<int:customer_id>/editar', views.edit_customer, name='edit_cliente'),
 ]
