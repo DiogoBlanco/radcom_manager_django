c62c2419d99b03471d53e34d65d8c183ec36a259
DiogoBlanco <diogoblanco@outlook.com.br>
2024-03-02 09:22:49 -0300

    contracts pagination

--- contratos/views.py
+++ contratos/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from clientes.models import Contract
 from .forms import ContractForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def contratos(request):
     contracts = Contract.objects.all().order_by('customer')
+
     total_value = sum(contract.value for contract in contracts)
+
+    paginator = Paginator(contracts, 10)
+    page = request.GET.get('page')
+    try:
+        contracts_list = paginator.page(page)
+    except PageNotAnInteger:
+        contracts_list = paginator.page(1)
+    except EmptyPage:
+        contracts_list = paginator.page(paginator.num_pages)
     context = {
-        'contracts': contracts,
+        'contracts_list': contracts_list,
         'total_value': total_value
     }
     return render(request, 'contratos/index.html', context)
