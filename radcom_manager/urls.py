ec99958c7b7dd80a527b107692463db8a9662cb3
DiogoBlanco <diogoblanco@outlook.com.br>
2024-03-02 20:44:32 -0300

    url changes and gitignore

--- radcom_manager/urls.py
+++ radcom_manager/urls.py
@@ -4,11 +4,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('', include('clientes.urls')),
-    path('contratos/', include('contratos.urls')),
-    path('atendimentos/', include('atendimentos.urls')),
-    path('anotacoes/', include('anotacoes.urls')),
-    path('arquivos/', include('arquivos.urls')),
+    path('app/admin/', admin.site.urls),
+    path('app/auth/', include('django.contrib.auth.urls')),
+    path('app/', include('clientes.urls')),
+    path('app/contratos/', include('contratos.urls')),
+    path('app/atendimentos/', include('atendimentos.urls')),
+    path('app/anotacoes/', include('anotacoes.urls')),
+    path('app/arquivos/', include('arquivos.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
